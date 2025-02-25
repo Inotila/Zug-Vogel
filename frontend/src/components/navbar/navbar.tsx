af456049fd6d4c0bf1a7994f644aeabf303c8910
inoati1991@gmail.com <inoati1991@gmail.com>
2025-02-25 01:24:47 +0100

    Added new pictures for logo, and updated nav to be properly spaced out but adjusting flex settins and col setting, and added style

--- frontend/src/components/navbar/navbar.tsx
+++ frontend/src/components/navbar/navbar.tsx
@@ -1,65 +1,101 @@
-// frontend/src/components/Navbar.tsx
-import React from 'react';
-import { Link } from 'react-router-dom';
-import '../../assets/css/index.css';
-import './navbar.css';
-
+import React from "react";
+import { Link } from "react-router-dom";
+import "../../assets/css/index.css";
+import logo from "../../assets/images/logo-pictures/nav-zug-vogel-color-logo.png";
+import "./navbar.css";
 
 const Navbar: React.FC = () => {
   return (
     <nav className="navbar navbar-expand-lg navbar-light">
       <div className="container-fluid">
-        {/* Logo */}
-        <div className="col-4">
-          <Link to="/" className="navbar-brand">Zug Vogel Namibia</Link>
-        </div>
-
-        {/* Search Bar */}
-        <div className="col-4">
-          <form className="d-flex">
-            <input
-              className="form-control"
-              type="search"
-              placeholder="Search"
-              aria-label="Search"
-            />
-          </form>
-        </div>
-
-        {/* Burger Menu */}
-        <div className="col-4 text-end">
-          <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-            <span className="navbar-toggler-icon"></span>
-          </button>
-          <div className="collapse navbar-collapse" id="navbarNav">
-            <ul className="navbar-nav">
-              <li className="nav-item">
-                <Link className="nav-link" to="/signup-login">Signup</Link>
-              </li>
-              <li className="nav-item">
-                <Link className="nav-link" to="/">Home</Link>
-              </li>
-              <li className="nav-item dropdown">
-                <Link className="nav-link dropdown-toggle" to="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                  Services
-                </Link>
-                <ul className="dropdown-menu" aria-labelledby="navbarDropdown">
-                  <li><Link className="dropdown-item" to="/#">Accommodation</Link></li>
-                  <li><Link className="dropdown-item" to="/#">Activities</Link></li>
-                </ul>
-              </li>
-              <li className="nav-item">
-                <Link className="nav-link" to="/services">About Us </Link>
-              </li>
-              <li className="nav-item">
-                <Link className="nav-link" to="/projects">Contact Us</Link>
-              </li>
-            </ul>
+        <div className="row w-100 align-items-center"> 
+          
+          {/* Logo */}
+          <div className="col-4 d-flex justify-content-start">
+            <Link to="/" className="navbar-brand">
+              <img className="nav-logo mx-3" src={logo} alt="Logo" />
+            </Link>
           </div>
-        </div>
+
+          <div className="col-4 d-flex justify-content-center">
+            <form className="d-flex w-100">
+              <input
+                className="form-control"
+                type="search"
+                placeholder="Search"
+                aria-label="Search"
+              />
+            </form>
+          </div>
+
+          {/* Burger Menu & Navbar Links */}
+          <div className="col-4 d-flex justify-content-end">
+            <button
+              className="navbar-toggler"
+              type="button"
+              data-bs-toggle="collapse"
+              data-bs-target="#navbarNav"
+              aria-controls="navbarNav"
+              aria-expanded="false"
+              aria-label="Toggle navigation"
+            >
+              <span className="navbar-toggler-icon"></span>
+            </button>
+
+            {/* Navbar Links (inside the same column) */}
+            <div className="collapse navbar-collapse justify-content-end" id="navbarNav">
+              <ul className="navbar-nav">
+                <li className="nav-item">
+                  <Link className="nav-link mx-3" to="/signup-login">
+                    Signup
+                  </Link>
+                </li>
+                <li className="nav-item">
+                  <Link className="nav-link" to="/">
+                    Home
+                  </Link>
+                </li>
+                <li className="nav-item dropdown">
+                  <Link
+                    className="nav-link dropdown-toggle mx-3"
+                    to="#"
+                    id="navbarDropdown"
+                    role="button"
+                    data-bs-toggle="dropdown"
+                    aria-expanded="false"
+                  >
+                    Services
+                  </Link>
+                  <ul className="dropdown-menu" aria-labelledby="navbarDropdown">
+                    <li>
+                      <Link className="dropdown-item" to="/#">
+                        Accommodation
+                      </Link>
+                    </li>
+                    <li>
+                      <Link className="dropdown-item" to="/#">
+                        Activities
+                      </Link>
+                    </li>
+                  </ul>
+                </li>
+                <li className="nav-item">
+                  <Link className="nav-link" to="/services">
+                    About Us
+                  </Link>
+                </li>
+                <li className="nav-item">
+                  <Link className="nav-link mx-3" to="/projects">
+                    Contact Us
+                  </Link>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </div> {/* End row */}
       </div>
     </nav>
   );
-}
+};
 
 export default Navbar;
