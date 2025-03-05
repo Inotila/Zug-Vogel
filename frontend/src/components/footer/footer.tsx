d494c8e2f5d23908fd1416607bb47cee1f713ad6
inoati1991@gmail.com <inoati1991@gmail.com>
2025-03-05 12:55:30 +0100

    Added style to footer and rearranged emelents to optimize space use

--- frontend/src/components/footer/footer.tsx
+++ frontend/src/components/footer/footer.tsx
@@ -4,38 +4,27 @@ import './assets/footer.css';
 
 const Footer: React.FC = () => {
   return (
-    <footer className="footer text-center mt-1">
+    <footer className="footer text-center mt-3">
       {/* Row 1: Links to Facebook and Instagram */}
       <div className="row">
         <div className="col">
-          <a href="https://www.facebook.com" target="_blank" rel="noopener noreferrer">
+          <a className='mx-3' href="https://www.facebook.com" target="_blank" rel="noopener noreferrer">
             Facebook
           </a>
-        </div>
-        <div className="col">
-          <a href="https://www.instagram.com" target="_blank" rel="noopener noreferrer">
+          <a className='mx-3' href="https://www.instagram.com" target="_blank" rel="noopener noreferrer">
             Instagram
           </a>
         </div>
       </div>
-
-      {/* Row 2: Property of Inovative Studios */}
+      {/* Row 2: Privacy Policy, Terms & Conditions, and Copyright */}
       <div className="row">
         <div className="col">
-          <p>Property of Zug Vogel Namibia Cc</p>
-        </div>
-      </div>
+          <div className='footter-copy-right-container mt-3'>
+            <a href="/privacy-policy">Privacy Policy</a>
+            <p className='mx-3'>&copy; {new Date().getFullYear()} Zug Vogel Namibia</p>
+            <a href="/terms-conditions">Terms & Conditions</a>
+          </div>
 
-      {/* Row 3: Privacy Policy, Terms & Conditions, and Copyright */}
-      <div className="row">
-        <div className="col">
-          <a href="/privacy-policy">Privacy Policy</a>
-        </div>
-        <div className="col">
-          <p>&copy; {new Date().getFullYear()} Inovative Studios</p>
-        </div>
-        <div className="col">
-          <a href="/terms-conditions">Terms & Conditions</a>
         </div>
       </div>
     </footer>
