3257f2c37cb47f9dc6587f2b01519a33d3e158e3
inoati1991@gmail.com <inoati1991@gmail.com>
2025-03-06 22:54:22 +0100

    Added style to make tour cards more responsive

--- frontend/src/components/tours/TourCard.tsx
+++ frontend/src/components/tours/TourCard.tsx
@@ -11,12 +11,19 @@ interface TourCardProps {
 const TourCard: React.FC<TourCardProps> = ({ title, image, description }) => {
     return (
         <div className="tour-card card mb-3">
-            <div className="card-body d-flex">
-                <img src={image} alt={title} className="tour-image me-3" />
-                <div className="tour-content">
+            <div className="card-body">
+                <div className="tour-header">
                     <h2 className="card-title">{title}</h2>
+                </div>
+                <img src={image} alt={title} className="tour-image" />
+                <div className="tour-content">
+                    <div className="tour-bigscreen-header">
+                        <h2 className="card-title">{title}</h2>
+                    </div>
                     <p className="tour-description">{description}</p>
-                    <Link to="/tour-details" className="btn tour-details-btn btn-outline-dark mt-2">Show more details</Link>
+                    <Link to="/tour-details" className="btn tour-details-btn btn-outline-dark mt-2">
+                        Show more details
+                    </Link>
                 </div>
             </div>
         </div>
