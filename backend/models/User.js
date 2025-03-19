fa9e1c90c5f880db42089eec2727d687ab5a25b5
inoati1991@gmail.com <inoati1991@gmail.com>
2025-03-19 13:29:11 +0100

    Added new phone number and language to user model

--- backend/models/User.js
+++ backend/models/User.js
@@ -4,6 +4,8 @@ const UserSchema = new mongoose.Schema({
   name: { type: String, required: true },
   email: { type: String, required: true, unique: true },
   password: { type: String, required: true },
+  phoneNumber: { type: String, default: '' },
+  preferredLanguage: { type: String, default: 'English' },
   interests: [{ type: String }],
   createdAt: { type: Date, default: Date.now }
 });
