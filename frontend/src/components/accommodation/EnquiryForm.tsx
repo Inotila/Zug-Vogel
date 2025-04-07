3c32e3cc72c824671b66ddf5fac038ea6e7b20d2
inoati1991@gmail.com <inoati1991@gmail.com>
2025-04-07 14:32:58 +0200

    Added german text to form

--- frontend/src/components/accommodation/EnquiryForm.tsx
+++ frontend/src/components/accommodation/EnquiryForm.tsx
@@ -11,10 +11,11 @@ const EnquiryForm: React.FC<EnquiryFormProps> = ({ onClose, accommodationTitle }
     const [name, setName] = useState('');
     const [email, setEmail] = useState('');
     const [NumberOfAdults, setAdults] = useState(1);
-    const [NumberOfChildren, setChildren] = useState(1);
+    const [NumberOfChildren, setChildren] = useState(0);
     const [arrivalDate, setArrivalDate] = useState('');
     const [departureDate, setDepartureDate] = useState('');
     const [isSubmitting, setIsSubmitting] = useState(false);
+    const today = new Date().toISOString().split("T")[0];
 
     useEffect(() => {
         const token = localStorage.getItem('token');
@@ -30,6 +31,17 @@ const EnquiryForm: React.FC<EnquiryFormProps> = ({ onClose, accommodationTitle }
 
     const handleSubmit = async (e: React.FormEvent) => {
         e.preventDefault();
+
+        if (new Date(arrivalDate) < new Date(today)) {
+            alert("Sorry, arrival date cannot be in the past. Please select a new date.");
+            return;
+        }
+
+        if (new Date(departureDate) <= new Date(arrivalDate)) {
+            alert("Sorry, departure date must be after the arrival date.Please select a new date.");
+            return;
+        }
+
         setIsSubmitting(true);
 
         const formData = {
@@ -70,8 +82,16 @@ const EnquiryForm: React.FC<EnquiryFormProps> = ({ onClose, accommodationTitle }
     return (
         <div className="enquiry-overlay">
             <div className="enquiry-form-container">
-                <h3>Enquiry Form</h3>
-                <form onSubmit={handleSubmit}>
+                <h3>Booking Enquiry: <br />{accommodationTitle}</h3>
+                <p className='form-disclosure-text'>Füllen Sie das untenstehende Formular aus
+                    <br /> und wir werden uns in Kürze mit einer Antwort auf Ihre Anfrage bei Ihnen melden.
+                    <br />
+                    Bitte beachten Sie, dass es sich hierbei nicht um eine Buchung,
+                    <br /> sondern um eine Buchungsanfrage handelt.
+                    <br />
+                    Vielen Dank für Ihr Interesse!
+                </p>
+                <form className='mt-1' onSubmit={handleSubmit}>
                     <label>
                         Name:
                         <input
@@ -82,7 +102,7 @@ const EnquiryForm: React.FC<EnquiryFormProps> = ({ onClose, accommodationTitle }
                         />
                     </label>
                     <label>
-                        Email:
+                        E-mail:
                         <input
                             type="email"
                             value={email}
@@ -91,7 +111,7 @@ const EnquiryForm: React.FC<EnquiryFormProps> = ({ onClose, accommodationTitle }
                         />
                     </label>
                     <label>
-                        Number of Adults:
+                        Anzahl Erwachsene:
                         <input
                             type="number"
                             min={1}
@@ -100,7 +120,7 @@ const EnquiryForm: React.FC<EnquiryFormProps> = ({ onClose, accommodationTitle }
                             required
                         />
                     </label><label>
-                        Number of children
+                        Anzahl der Kinder
                         <input
                             type="number"
                             min={0}
@@ -110,29 +130,31 @@ const EnquiryForm: React.FC<EnquiryFormProps> = ({ onClose, accommodationTitle }
                         />
                     </label>
                     <label>
-                        Arrival Date:
+                        Ankunftsdatum:v
                         <input
                             type="date"
+                            min={today}
                             value={arrivalDate}
                             onChange={(e) => setArrivalDate(e.target.value)}
                             required
                         />
                     </label>
                     <label>
-                        Departure Date:
+                        Abreisedatum:
                         <input
                             type="date"
                             value={departureDate}
+                            min={arrivalDate || today}
                             onChange={(e) => setDepartureDate(e.target.value)}
                             required
                         />
                     </label>
                     <div className="button-group">
                         <button type="submit" className="btn main-btn" disabled={isSubmitting}>
-                            {isSubmitting ? 'Sending...' : 'Submit'}
+                            {isSubmitting ? 'Senden...' : 'Verwandt'}
                         </button>
                         <button type="button" className="btn sub-btn cancel-btn" onClick={onClose}>
-                            Cancel
+                            Stornieren
                         </button>
                     </div>
                 </form>
