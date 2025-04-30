13b3478a78a4015ad168d20e46ca28a952e126bf
inoati1991@gmail.com <inoati1991@gmail.com>
2025-04-30 20:29:01 +0200

    removed unneeded code

--- frontend/src/components/tours/tourDetails.tsx
+++ frontend/src/components/tours/tourDetails.tsx
@@ -1,8 +1,8 @@
 import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import "../../assets/css/index.css";
-import "../../assets/css/serviceDetails.css";
 import "./assets/css/tours.css";
+import "../../assets/css/serviceDetails.css";
 import "../../assets/css/serviceStyle.css";
 import { fetchTourBySlug } from "../../services/contentfulService";
 
