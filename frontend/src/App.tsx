7a41c36879e07a374e096acdbd1c81b652397224
inoati1991@gmail.com <inoati1991@gmail.com>
2025-04-30 20:29:35 +0200

    added lazy load to only load pages when they are in use

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,20 +1,21 @@
-import React from 'react';
+import React, { lazy, Suspense } from 'react';
 import './App.css';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Navbar from './components/navbar/navbar';
 import Footer from './components/footer/footer';
-import HomePage from './components/home/home';
-import AccomodationPage from './components/accommodation/accommodation';
-import TourPage from './components/tours/tours';
-import TourDetailsPage from './components/tours/tourDetails';
-import AboutNamibiaPage from './components/aboutNambia/aboutNamibia';
-import AboutUsPage from './components/aboutUs/aboutUs'
-import SignUpPage from './components/userAuth/signup'
-import LoginPage from './components/userAuth/login'
-import ProfilePage from './components/userAuth/profile'
-import ActivitiesPage from './components/activities/activities';
-import AccomodationDetailsPage from './components/accommodation/accommodationDetails';
 
+// Lazy-loaded components
+const HomePage = lazy(() => import('./components/home/home'));
+const AccomodationPage = lazy(() => import('./components/accommodation/accommodation'));
+const TourPage = lazy(() => import('./components/tours/tours'));
+const TourDetailsPage = lazy(() => import('./components/tours/tourDetails'));
+const AboutNamibiaPage = lazy(() => import('./components/aboutNambia/aboutNamibia'));
+const AboutUsPage = lazy(() => import('./components/aboutUs/aboutUs'));
+const SignUpPage = lazy(() => import('./components/userAuth/signup'));
+const LoginPage = lazy(() => import('./components/userAuth/login'));
+const ProfilePage = lazy(() => import('./components/userAuth/profile'));
+const ActivitiesPage = lazy(() => import('./components/activities/activities'));
+const AccomodationDetailsPage = lazy(() => import('./components/accommodation/accommodationDetails'));
 
 const App: React.FC = () => {
   return (
@@ -22,21 +23,21 @@ const App: React.FC = () => {
       <div className="App">
         <Navbar />
         <div className="content">
-          <Routes>
-            {/* HomePage component */}
-            <Route path="/" element={< HomePage />} />
-            {/* AccomodationPage route */}
-            <Route path="/unterkunft" element={< AccomodationPage />} />
-            <Route path="/touren" element={< TourPage />} />
-            <Route path="/touren/:slug" element={< TourDetailsPage />} />
-            <Route path="/überNamibia" element={< AboutNamibiaPage />} />
-            <Route path="/überUns" element={< AboutUsPage />} />
-            <Route path="/signup" element={< SignUpPage />} />
-            <Route path="/login" element={< LoginPage />} />
-            <Route path="/profile" element={< ProfilePage />} />
-            <Route path="/aktivitäten" element={< ActivitiesPage />} />
-            <Route path="/unterkunft/:slug" element={< AccomodationDetailsPage />} />
-          </Routes>
+          <Suspense fallback={<div>Loading...</div>}>
+            <Routes>
+              <Route path="/" element={<HomePage />} />
+              <Route path="/unterkunft" element={<AccomodationPage />} />
+              <Route path="/touren" element={<TourPage />} />
+              <Route path="/touren/:slug" element={<TourDetailsPage />} />
+              <Route path="/überNamibia" element={<AboutNamibiaPage />} />
+              <Route path="/überUns" element={<AboutUsPage />} />
+              <Route path="/signup" element={<SignUpPage />} />
+              <Route path="/login" element={<LoginPage />} />
+              <Route path="/profile" element={<ProfilePage />} />
+              <Route path="/aktivitäten" element={<ActivitiesPage />} />
+              <Route path="/unterkunft/:slug" element={<AccomodationDetailsPage />} />
+            </Routes>
+          </Suspense>
         </div>
         <Footer />
       </div>
