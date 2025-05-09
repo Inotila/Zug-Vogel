8be7a85fd8793def9a1a6e459957c250677fe2f2
inoati1991@gmail.com <inoati1991@gmail.com>
2025-05-09 02:49:46 +0200

    Added logo  images and update nav logo img

--- frontend/src/components/navbar/navbar.tsx
+++ frontend/src/components/navbar/navbar.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { Link, useNavigate } from "react-router-dom";
 import "../../assets/css/index.css";
-import logo from "../../assets/images/logo-images/nav-zug-vogel-color-logo.png";
+import logo from "../../assets/images/logo-images/nav-logo.png";
 import "./navbar.css";
 
 const Navbar: React.FC = () => {
